1b83c6ef4846c735ca1f98ad665171809b6042e8
Tulio <tulio@century.net.br>
2018-01-30 15:07:42 -0200

    First commit

---
+++ Configurer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Essentials:
+    def __init__(self):
+        try:
+            with open('setup.conf', 'r') as syspath:
+                output = syspath.read().strip()
+                out = ''
+            for line in output.strip().splitlines():
+                if not line.strip().startswith("#"):
+                    out += line.rstrip() + '\n'
+            o = out.strip().splitlines()
+            self.rrd_path = o[0].split(' := ')[1]
+            self.host = o[1].split(' := ')[1]
+            self.daemon_username = o[2].split(' := ')[1].split('|')[0]
+            self.daemon_groupname = o[2].split(' := ')[1].split('|')[1]
+            if not self.rrd_path.endswith('/'):
+                self.rrd_path = self.rrd_path + '/'
+        except FileNotFoundError:
+            print('\nError[1]: setup.conf file not found!\n')
+            with open('setup.conf.sample', 'r') as syspath:
+                print(syspath.read().strip())
+            exit(1)
---
+++ main.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+import Configurer
+import subprocess
+import os
+import rrdtool
+from daemon import runner
+import time
+import grp
+import pwd
+
+conf = Configurer.Essentials()
+rrd_path = conf.rrd_path
+hostname = conf.host
+d_user = conf.daemon_username
+d_group = conf.daemon_groupname
+
+
+def ping(host):
+    output = subprocess.Popen(
+        ["ping", "-c", "5", "-W", "1", host],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    out, error = output.communicate()
+    return out
+
+
+def status(result):
+    state = None
+    for line in result.decode().strip().splitlines():
+        if 'packets transmitted' in line:
+            if '100.0%' == line.strip().split()[6]:
+                print(str(time.time()) + '\tLost conn')
+                state = False
+            else:
+                print(str(time.time()) + '\tOK')
+                state = True
+    if state is None:
+        print('Error[2]: Inconsistent ping output.')
+        exit(2)
+    else:
+        return state
+
+
+def update(file_path, host):
+    # now = int(time.time())
+    # According to docs, it is possible to use 'N' in place of now (time.time())
+    if status(ping(host)) is True:
+        # rrdtool.update(file_path, str(now) + ':1')
+        rrdtool.update(file_path, 'N:1')
+    else:
+        # rrdtool.update(file_path, str(now) + ':0')
+        rrdtool.update(file_path, 'N:0')
+
+
+class Daemon(object):
+    def __init__(self):
+        """Initialize Daemon."""
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/tmp/rrdpinger_stdout.log'
+        self.stderr_path = '/tmp/rrdpinger_stderr.log'
+        self.pidfile_path = '/tmp/rrdpinger_daemon.pid'
+        self.pidfile_timeout = 8
+
+    @staticmethod
+    def run():
+        full_path = rrd_path + hostname + '/icmp_uptime.rrd'
+        if os.path.exists(full_path) is False:
+            print('\nerror\n')
+            rrdtool.create(full_path,
+                           'DS:status:GAUGE:600:U:U',
+                           'RRA:AVERAGE:0.5:1:600',
+                           'RRA:AVERAGE:0.5:6:700',
+                           'RRA:AVERAGE:0.5:24:775',
+                           'RRA:AVERAGE:0.5:288:797')
+        try:
+            while 1:
+                update(full_path, hostname)
+                time.sleep(30)  # Change to 300 for production env
+        except KeyboardInterrupt:
+            print('Exiting[10]: Keyboard interrupted.')
+            exit(10)
+
+
+if __name__ == '__main__':
+    app = Daemon()
+    daemon_runner = runner.DaemonRunner(app)
+    daemon_gid = grp.getgrnam(d_group).gr_gid
+    daemon_uid = pwd.getpwnam(d_user).pw_uid
+    daemon_runner.daemon_context.gid = daemon_gid
+    daemon_runner.daemon_context.uid = daemon_uid
+    daemon_runner.do_action()
